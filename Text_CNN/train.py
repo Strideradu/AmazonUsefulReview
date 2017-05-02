4e00e7915e4035b0f89b283b5caaf76d41d0aced
Strideradu <dunan00001@hotmail.com>
2017-05-02 14:45:42 -0400

    match embedding dimension

--- Text_CNN/train.py
+++ Text_CNN/train.py
@@ -20,7 +20,7 @@ tf.flags.DEFINE_string("word2vec_file", "/mnt/home/dunan/Learn/Class/CSE842/Data
 #tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 100, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
